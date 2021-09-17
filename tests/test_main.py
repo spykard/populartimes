648f89cd12ef713cfaff843232edbe2b6d91dbf4
spykard <kardakis@ceid.upatras.gr>
2021-09-17 19:49:59 +0300

    Add Main Test File

---
+++ tests/test_main.py
@@ -0,0 +1,59 @@
+''' Query Google's PopularTimes in circles, given latitute and longtitude positions and retrieve data for places/stores '''
+import configparser
+import populartimes
+
+# Config Parsing
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+
+mode = eval(config['Settings']['mode'])
+
+if (mode == 1):
+    # Get Data by ID
+    api_key = config['Settings']['api_key']
+    place_id = config['Settings']['place_id']
+    data = populartimes.get_id(api_key=api_key, place_id=place_id)
+    data = [data]
+
+# Warning can be incredibly costly
+elif (mode == 2):
+    # Get Data by Circular Location
+    api_key = config['Settings']['api_key']
+    types = config['Settings']['types'].split(",")
+    p1 = eval(config['Settings']['p1'])
+    p2 = eval(config['Settings']['p2'])
+    radius = eval(config['Settings']['radius'])
+    all_places = eval(config['Settings']['all_places'])
+
+    data = populartimes.get(api_key=api_key, types=types, p1=p1, p2=p2, n_threads=20, radius=radius, all_places=all_places)
+
+    print(len(data))
+    for place in data:
+        print(place['name'])
+        print(place['address'])
+        print(place['types'])
+        print(str(place['rating']) + " (" + str(place['rating_n']) + ")")
+        if "current_popularity" in place:
+            print(place['current_popularity'])
+        else:
+            print("N/A")
+        print(place['populartimes'])
+        if "time_spent" in place:
+            print(place['time_spent'])
+        else:
+            print("N/A")
+        print()
+
+elif (mode == 3):
+    # Get Data by Crawling Google Maps itself, using two keywords which are combined
+    # returns: (rating, rating_n, popular_times, current_popularity, time_spent)
+    search_name = config['Settings']['search_name']
+    search_address = config['Settings']['search_address']
+
+    data = populartimes.get_popular_times_by_crawl(name=search_name, address=search_address)
+
+    print(search_name + " " + search_address)
+    print("rating: " + str(data[0]) + " (" + str(data[1]) + ")")
+    print("popular_times: " + str(data[2]))
+    print("current_popularity: " + str(data[3]))
+    print("time_spent: " + str(data[4]))
